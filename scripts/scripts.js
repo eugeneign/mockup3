4d5acfe84a4a9462e8945713c18df4c4cc0bddd4
eugeneign <49946057+eugeneign@users.noreply.github.com>
2019-12-23 13:50:01 +0300

    23.12

---
+++ scripts/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready
+
+
+  $('select').niceSelect();
+  
+$('.nav__item-mobile').on("click", function (e) {
+  e.preventDefault();
+  $('.nav__item, .nav__item-mobile').toggleClass('is-opened')
+});
